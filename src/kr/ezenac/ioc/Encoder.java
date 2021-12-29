6a086847cd7a5a803b1fa4865593bb0d3a4c1189
ezenaclee <leedlsrl1@gmail.com>
2021-12-29 11:53:00 +0900

    has a 관계로 클래스 추가

---
+++ src/kr/ezenac/ioc/Encoder.java
@@ -0,0 +1,14 @@
+package kr.ezenac.ioc;
+
+public class Encoder {
+
+    private IEncoder iEncoder;
+
+    public Encoder() {
+        this.iEncoder = new Base64Encoder();
+    }
+
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+}
